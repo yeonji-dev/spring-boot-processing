f086f86dcb6b8b8bd57217c7cc0b68d10493ea4c
yeonji-dev <lilianaaa804@gmail.com>
2023-04-25 17:20:55 +0900

    initial

---
+++ src/main/java/com/yeonjidev/springbootproceccing/SpringBootProceccingApplication.java
@@ -0,0 +1,13 @@
+package com.yeonjidev.springbootproceccing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProceccingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProceccingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yeonjidev/springbootproceccing/SpringBootProceccingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeonjidev.springbootproceccing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProceccingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
